2a2d92abe4d89749fffbf9f5fde215395b38719e
zepedaG06 <jazepeda@uamv.edu.ni>
2026-09-16 17:25:37 -0600

    Clase Navegacion terminada y paquete controller terminado

--- src/main/java/ni/edu/uam/sistemas_clientes/controller/NavegacionService.java
+++ src/main/java/ni/edu/uam/sistemas_clientes/controller/NavegacionService.java
@@ -1,4 +1,36 @@
-package ni.edu.uam.sistemas_clientes.controller;
+package ni.edu.uam.sistemasolicitudesclientes.service;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NavegacionService {
+
+    public static void cambiarVentana(
+            Stage stage,
+            String archivoFXML,
+            String titulo)
+            throws IOException {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        NavegacionService.class
+                                .getResource(
+                                        "/ni/edu/uam/sistemasolicitudesclientes/"
+                                                + archivoFXML
+                                )
+                );
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle(titulo);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
